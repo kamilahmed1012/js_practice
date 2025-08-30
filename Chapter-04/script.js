4d13509f31c07a0b730399707fb699526faed808
kamilahmed10121 <225457959+kamilahmed1012@users.noreply.github.com>
2025-08-30 23:07:26 +0500

    n

---
+++ Chapter-04/script.js
@@ -0,0 +1,32 @@
+// Problem-01
+// what will the following print in javaScript?
+// console.log('kam\"'.length) //4
+
+// Problem-02
+
+// Explore the includes, start with & endsWith function of string
+// let str = "Hello, welcome to the world of JavaScript";
+// console.log(str.includes("welcome")); // true
+// console.log(str.startsWith("Hello")); // true
+// console.log(str.endsWith("JavaScript")); // true
+
+// problem-03
+
+// write a program to convert a given string to lowercase
+
+// let string = "Hello, World!";
+// let lowerCase = string.toLowerCase();
+
+// Problem-04
+
+// extract the amount out of this string "please give RS 1000"
+
+// let str = "please give RS 1000";
+// let amount =str.slice(15);
+// console.log(amount);
+
+// Problem-05
+// Try to change the 4th character of a given string.  were you able to do it?
+// let str = "hello warld";
+// str[7] = "o"; // not possible
+// console.log(str); // hello warld
