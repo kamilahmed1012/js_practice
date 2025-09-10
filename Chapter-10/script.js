64075723fa17375bf489e97ae0ebc024270fbfdb
kamilahmed10121 <225457959+kamilahmed1012@users.noreply.github.com>
2025-09-10 10:48:28 +0500

    mm

--- Chapter-10/script.js
+++ Chapter-10/script.js
@@ -1,21 +1,42 @@
-fetch('https://api.animechan.io/v1/quotes/random')
-    .then(response => response.json())
-    .then(quote => console.log(quote))
+// Problem - 01
 
-    
-let obj = {
-	    status: 'success',
-	    data: {
-		    content: 'The final door is about to open! And I am the one opening it! Then the world that we know of will come to an end! This world of insatiable desires will end!',
-		    anime: {
-			    id: 575,
-			    name: 'Mobile Suit Gundam SEED',
-			    altName: 'Kidou Senshi Gundam SEED'
-		    },
-		    character: {
-			    id: 1486,
-			    name: 'Rau Le Creuset'
-		    }
-        }
+const url = 'https://potterapi-fedeperin.vercel.app/en/spells' 
+
+let response = fetch(url)
+
+response.then((v)=>{
+    return v.json()
+}).then((contests)=> {
+    console.log(contests)
+    ihtml = ''
+    for(item in contests){
+        console.log(contests[item])
+         ihtml += `<div class="row">
+            <div class="card m-3" style="width: 18rem;">
+                <img src="./ChatGPT Image Aug 29, 2025, 09_53_50 PM.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${contests[item].spell}</h5>
+                    <p class="card-text">${contests[item].use}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>`
+        cardContainer.innerHTML = ihtml
     }
+})
 
+// Problem - 02
+let getNote = localStorage.getItem('notes')
+alert('Your note is '+   getNote)
+
+let note = prompt('Enter Your Notes')
+if(note){
+    localStorage.setItem('notes', note)
+}
+
+let deleteNote = confirm('do you want to delete')
+
+if(deleteNote){
+    localStorage.removeItem('notes')
+    alert('Notes delete seccessfully')
+}
